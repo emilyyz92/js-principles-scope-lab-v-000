f5bcd01cf41fc3f3310e32021dfbb5d039081ca0
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-10 18:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Write your solution in this file!
 customerName = 'bob';
+
 function upperCaseCustomerName(name) {
   return name.toUppercase();
+}
+
+function bestCustomer() {
+  return undefined;
 }
