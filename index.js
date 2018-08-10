2011bbd4136f547685fb50d17126a67a66526c4e
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-10 18:47:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 customerName = `bob`;
 
 function upperCaseCustomerName() {
-  return customerName = customerName.toUppercase;
+  customerName = customerName.toUppercase;
 }
 
 function bestCustomer() {
